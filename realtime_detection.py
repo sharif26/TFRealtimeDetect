e161b813c2d85109a805c24a1d139e52b1a97414
Sharif Ahmed <shiarif@gmail.com>
2017-11-07 17:03:45 -0500

    graph added

--- realtime_detection.py
+++ realtime_detection.py
@@ -22,7 +22,7 @@ MODEL_FILE = MODEL_NAME + '.tar.gz'
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_CKPT = 'data/frozen_inference_graph.pb'
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 NUM_CLASSES = 90
@@ -30,15 +30,6 @@ NUM_CLASSES = 90
 path = os.path.dirname(os.path.abspath(__file__))
 file_location = os.path.join(path, MODEL_FILE)
 graph_location = os.path.join(path, PATH_TO_CKPT)
-
-if not os.path.exists(file_location):   #if there is no local copy or local copy is empty then go get it...
-  opener = urllib.request.URLopener()
-  opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-  tar_file = tarfile.open(MODEL_FILE)
-  for file in tar_file.getmembers():
-    file_name = os.path.basename(file.name)
-    if 'frozen_inference_graph.pb' in file_name:
-      tar_file.extract(file, os.getcwd())
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
